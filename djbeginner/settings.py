ce4dff9fea2fc523f61bdf64e3d4c6651b3dc5f4
Phillip Jhon Lumod <philliplumod@gmail.com>
2025-02-09 21:30:09 +0800

    Update database settings to use PostgreSQL with service and passfile options

--- djbeginner/settings.py
+++ djbeginner/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'djbeginner.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "my_service",
+            "passfile": ".my_pgpass",
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
